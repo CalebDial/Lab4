64916ad312814c578902267f2cffa6d281fb87ab
Caleb <Caleb@192.168.1.110>
2019-10-15 18:51:10 -0500

    Final Committ

--- src/main/java/CarClass.java
+++ src/main/java/CarClass.java
@@ -9,5 +9,52 @@
  * @author Caleb
  */
 public class CarClass {
-    
+  private final String myMake;
+    private final int myYearModel;
+    private double mySpeed;
+    CarClass(int yearModel, String make){
+        this.myYearModel = yearModel;
+        this.myMake = make;
+        this.mySpeed = 0;
+    }
+    public String getMyMake() {
+        return myMake;
+    }
+    public int getMyYearModel() {
+        return myYearModel;
+    }
+    public double getMySpeed() {
+        return mySpeed;
+    }
+    public void accelerate() {
+        this.mySpeed = this.mySpeed + 5;
+    }
+    public void brake() {
+        this.mySpeed = this.mySpeed - 5;
+    }
+    public static void main(String [] args) {
+        
+        CarClass myLambo = new CarClass(2019, "Lamborghini");
+        
+ 
+        System.out.println("Buckle up!....");
+        
+        int xLoop = 0; 
+        while (xLoop <= 4) 
+        { 
+            myLambo.accelerate();
+            System.out.println(myLambo.getMySpeed() + "MPH");
+ 
+            xLoop++; 
+        } 
+       
+        xLoop = 0;
+        while (xLoop <= 4) 
+        { 
+            myLambo.brake();
+            System.out.println(myLambo.getMySpeed() + "MPH");
+  
+            xLoop++; 
+        } 
+        }
 }
--- src/main/java/CircleClass.java
+++ src/main/java/CircleClass.java
@@ -8,6 +8,45 @@
  *
  * @author Caleb
  */
+import java.util.Scanner;
 public class CircleClass {
-    
+     private double radius;
+    private final double PI = 3.14159;
+    CircleClass(double radius){
+        this.radius = radius;
+    }
+    CircleClass(){
+        this.radius = 0.00;
+    }
+    public void putRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public double getCircumference(){
+        return this.PI * 2.0 * this.radius;
+    }
+    public double getArea(){
+        return this.PI * this.radius * this.radius;
+    }
+    public double getDiameter(){
+        return this.radius * 2;
+    }
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Please enter a radius for a circle: ");
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble();
+        
+        
+        CircleClass newCircle = new CircleClass(radius);
+        System.out.println("The circle you entered has a circumference of " + newCircle.getCircumference());
+        
+        System.out.println("The circle you entered has an area of " + newCircle.getArea());
+        System.out.println("The circle you entered has a diameter of " + newCircle.getDiameter());
+
+    }
 }
